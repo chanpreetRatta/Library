3210076e301fa915bc2bba1358e3d82a5e146d3c
Devolpment <devolpment@MacBook-Pro-2.local>
2023-04-18 17:47:29 -0700

    edit the UI layout

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
-console.log("je;;p");
+const myLibrary = [
+  {
+    title: "Meri Kitab",
+    author: "Chanpreet",
+    Pages: 250,
+    isRead: true,
+  },
+];
+
+let bookShelf = document.querySelector(".book-shelf");
+
+console.log(bookShelf);
